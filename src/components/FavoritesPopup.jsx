9dfaa6ce270fd11e01c349eb44182acaec9ecc63
JLegends <113209667+JLegends@users.noreply.github.com>
2025-03-23 14:41:18 -0600

    painting popup templated

--- src/components/FavoritesPopup.jsx
+++ src/components/FavoritesPopup.jsx
@@ -23,12 +23,18 @@ const FavoritesPopup = forwardRef(({toggleDialog}, ref) => {
             }}
             > 
             <div className="w-full h-120 bg-[#212121] rounded-xl" >
-                <div className="relative bg-white">
-                    { /*
-                    <ArtistList artist={}/>
-                    <GalleryList gallery={}/>
-                    <PaintingList painting={}/>
-                        */ }
+                <div className="relative h-full bg-white">
+                    <div className="flex flex-shrink h-full p-2 bg-[#000000]">
+                        <div className="flex items-center justify-center bg-[#000000] pr-2 w-1/3">
+                            { /*  <ArtistList artist={}/> */ }
+                        </div> 
+                        <div className="flex w-1/3 rounded-xl m-2 bg-linear-to-t from-[#121212] to-button-focus">
+                            { /*  <GalleryList gallery={}/> */ }
+                        </div>
+                        <div className="flex text-white w-1/3 rounded-xl m-2 bg-linear-to-t from-[#121212] to-[#212121] p-4">
+                            { /*  <PaintingList painting={}/> */ }
+                        </div>
+                    </div>
                     <button onClick={toggleDialog} className="absolute top-4 right-4 py-2 px-4 bg-button text-button-focus hover:bg-button-focus hover:text-white rounded-full">X</button>
                 </div>
             </div>
